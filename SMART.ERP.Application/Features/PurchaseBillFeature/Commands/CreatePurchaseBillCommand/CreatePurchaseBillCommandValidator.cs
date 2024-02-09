185677fda75327c7a46406d84126ce46a39865b3
José Cubas <58412645+Splitzer26@users.noreply.github.com>
2024-02-09 14:59:42 -0600

    feat: new endpoint and command for purchasebills

---
+++ SMART.ERP.Application/Features/PurchaseBillFeature/Commands/CreatePurchaseBillCommand/CreatePurchaseBillCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMART.ERP.Application.Features.PurchaseBillFeature.Commands.CreatePurchaseBillCommand
+{
+    public class CreatePurchaseBillCommandValidator : AbstractValidator<CreatePurchaseBillCommand>
+    {
+        public CreatePurchaseBillCommandValidator() {
+        }
+    }
+}
