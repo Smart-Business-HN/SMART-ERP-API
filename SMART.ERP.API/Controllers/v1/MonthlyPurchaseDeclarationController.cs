3c0f057a89ec1769125be1e7bfb7ac3264a61546
José Cubas <58412645+Splitzer26@users.noreply.github.com>
2024-05-16 14:30:09 -0600

    feat: new endpoints to get Monthly Purchase Declarations

--- SMART.ERP.API/Controllers/v1/MonthlyPurchaseDeclarationController.cs
+++ SMART.ERP.API/Controllers/v1/MonthlyPurchaseDeclarationController.cs
@@ -1,6 +1,9 @@
 ﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SMART.ERP.Application.Features.MonthlyPurchaseDeclarationFeature.Commands.CreateMonthlyPurchaseDeclarationCommand;
+using SMART.ERP.Application.Features.MonthlyPurchaseDeclarationFeature.Queries;
+using SMART.ERP.Application.Parameters;
 
 namespace SMART.ERP.API.Controllers.v1
 {
@@ -8,9 +11,24 @@ namespace SMART.ERP.API.Controllers.v1
     public class MonthlyPurchaseDeclarationController : BaseApiController
     {
         [HttpPost("Create")]
+        [Authorize]
         public async Task<IActionResult> Send([FromBody] CreateMonthlyPurchaseDeclarationCommand command)
         {
             return Ok(await Mediator.Send(command));
+        }
+        [HttpGet("GetAll")]
+        [Authorize]
+        public async Task<IActionResult> GetAll([FromQuery] RequestParameter filter)
+        {
+            return Ok(await Mediator.Send(new GetAllMonthlyPurchaseDeclarationQuery
+            {
+                Parameter = filter.Parameter,
+                PageNumber = filter.PageNumber,
+                PageSize = filter.PageSize,
+                Order = filter.Order,
+                Column = filter.Column,
+                All = filter.All
+            }));
         }
     }
 }
